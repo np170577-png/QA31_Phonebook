df303c8603d5a85934a47190f5232aae727c5403
np170577-png <np170577@gmail.com>
2025-11-17 17:31:12 +0200

    Add Contact

---
+++ src/main/java/manager/ApplicationManager.java
@@ -0,0 +1,23 @@
+package manager;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ApplicationManager {
+    WebDriver wd;
+    HelperUser helperUser;
+
+    public void init() {
+        wd = new ChromeDriver();
+        wd.navigate().to("https://telranedu.web.app/");
+        helperUser = new HelperUser(wd);
+    }
+
+    public HelperUser getHelperUser() {
+        return helperUser;
+    }
+
+    public void stop() {
+        wd.quit();
+    }
+}
